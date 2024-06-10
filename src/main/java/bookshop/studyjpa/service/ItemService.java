16628561f70134ecd40d8ecaa8f4e6ada3bf09e0
dltkqnr <dltkqnr@naver.com>
2024-06-10 23:59:30 +0900

    # 상품 controller 개발
    - 상품 등록
    - 상품 목록
    - 상품 수정

--- src/main/java/bookshop/studyjpa/service/ItemService.java
+++ src/main/java/bookshop/studyjpa/service/ItemService.java
@@ -4,15 +4,18 @@ import bookshop.studyjpa.domain.item.Item;
 import bookshop.studyjpa.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    @Transactional
     public void save(Item item){
         itemRepository.save(item);
     }
@@ -23,5 +26,14 @@ public class ItemService {
 
     public Item findOne(Long id){
         return itemRepository.findOne(id);
+    }
+
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity ){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        return findItem;
     }
 }
